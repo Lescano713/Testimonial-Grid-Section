a9610fbba89bbef6b8f2233896a6146d43cd2de1
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-07-27 01:40:00 -0500

    desktop-version

--- index.js
+++ index.js
@@ -10,21 +10,40 @@ getElements(data => {
     data.forEach(element => {
         let section = document.createElement("section");
         section.classList.add("testimonial-container");
-        let div = document.createElement("div");
-        div.classList.add("student-info")
-        let h1 = document.createElement("h1");
-        h1.textContent = element.studentName;
-        let p = document.createElement("p");
-        p.textContent = element.rol;
-        div.appendChild(h1);
-        div.appendChild(p);
+        section.classList.add(element.studentName.toLowerCase().replace(/ /g, '-'))
+        
+        let divInfo = document.createElement("div");
+        divInfo.classList.add("student-info")
+        let divText = document.createElement("div");
+        divText.classList.add("text");
+        divInfo.appendChild(divText);
+
+        let imageStudent = document.createElement("img");
+        imageStudent.src = element.studentImage;
+        let h1Name = document.createElement("h1");
+        h1Name.textContent = element.studentName;
+        let pRol = document.createElement("p");
+        pRol.textContent = element.rol;
+        divInfo.appendChild(imageStudent);
+        divText.appendChild(h1Name);
+        divText.appendChild(pRol);
+        
+        let pResume = document.createElement("p");
         let bResume = document.createElement("b");
         bResume.textContent = element.resume;
-        let qTestimonial = document.createElement("b");
+        pResume.appendChild(bResume)
+
+        let pTestimonial = document.createElement("p");
+        let qTestimonial = document.createElement("q");
         qTestimonial.textContent = element.testimonial;
-        section.appendChild(div)
+        pTestimonial.appendChild(qTestimonial);
+        
+        section.appendChild(divInfo)
         section.appendChild(bResume);
         section.appendChild(qTestimonial);
+
+        section.style.backgroundColor = element.color;
+        section.style.color = element.colorFont;
         main.appendChild(section);
     });
 }) 
